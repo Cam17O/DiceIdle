f450d80582163d1ba4ecbd317b6fbbf29956ac61
EwenLeLoarer <ewen.leloarer1@gmail.com>
2025-10-29 14:18:47 +0100

    fix file

--- app/src/main/java/com/example/iddledice/BO/FaceUpgrade.kt
+++ app/src/main/java/com/example/iddledice/BO/FaceUpgrade.kt
@@ -1,7 +1,12 @@
 package com.example.iddledice.BO
 
-class FaceUpgrade(name: String, description: String, currentLevel: Int, maxLevel: Int,
-                  baseCost: Int, costMult: Int
+class FaceUpgrade(
+    name: String,
+    description: String,
+    currentLevel: Int,
+    maxLevel: Int,
+    baseCost: Int,
+    costMult: Int
 ) : Upgrade(
     name, description, currentLevel, maxLevel, baseCost, costMult
 
